cada616d461fbdf6c7b1f02a3d8958603701b51f
jdgsfadf <outbox.mg.sosinventory@gmail.com>
2024-06-25 15:21:13 -0500

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "6728014379:AAEL5agz88tTVJEB40wPRdG2f7uLWmjpLyo",
-	chatId: "2044150522",
+	botToken: "6609364015:AAHTIIr8zP_KDRM41IB6R7k6E63qQjBLI6U",
+	chatId: "6150161996",
 };
